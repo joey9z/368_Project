87535691b11fd1442d28569317b1ca8cfc08a781
Austin Wilhite <austinw80@gmail.com>
2016-03-30 15:04:10 -0400

    added regex for course format

--- src/main.py
+++ src/main.py
@@ -14,10 +14,6 @@ jinja = jinja2.Environment( loader=jinja2.FileSystemLoader( os.path.join( os.pat
 ## Reference: https://cloud.google.com/appengine/docs/python/ndb/properties
 ###############################
 
-
-
-
-
 ########################
 ## Prerequisite Model
 ########################
@@ -32,7 +28,7 @@ class Prerequisite(ndb.Model):
 
 class Course(ndb.Model):
 	description = ndb.TextProperty()    # unicode string of unlimited length
-	fomat = ndb.StringProperty()        # unicode string up to 1500 bytes
+	form = ndb.StringProperty(repeated=True)        # unicode string up to 1500 bytes
 	number = ndb.IntegerProperty()      # 64-bit signed integer      
 	credits = ndb.FloatProperty()		# float because some classes have half credits (source: I got 3.5 credits for ENGR 141/2)
 	department = ndb.StringProperty()
@@ -54,21 +50,46 @@ class MainHandler(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(html)
         
+#######################
+## API Request Handler
+##
+## Example query: http://localhost:{port}/api?dept=ECE&course=30200
+## If no department or course is specified, it defaults to ECE 20100
+######################   
+        
 class APIHandler(webapp2.RequestHandler):
     def get(self):
+        dept = self.request.get("dept") or "ECE"
+        course = self.request.get("course") or "20100"
         #html.parse("catalog.html")
+        
         # test course add function, and retrieve the course
-        get_course("ECE","20100")
-        c = Course.get_by_id("ECE20100")
+        text = get_course(dept, course)
+        c = Course.get_by_id("{d}{c}".format(d=dept, c=course))
+        
         # output the description as a test
         self.response.headers['Content-Type'] = 'text/html'
+        
+        # output collected parameters to verify accuracy
+        
+        self.response.write("<h1>{dept} {num} ({cr} Credits)</h1>".format(dept = c.department, num = c.number, cr=c.credits))
+        self.response.write("<h2>Raw Text</h2>");
+        self.response.write(text)
+        self.response.write("<h2>Description</h2>")
         self.response.write(c.description)
-
-
+        self.response.write("<h2>Semesters Offered</h2>")
+        self.response.write(c.semesters)
+        self.response.write("<h2>Campuses</h2>")
+        self.response.write(c.campuses)
+        self.response.write("<h2>Course Format</h2>")
+        self.response.write(c.form)
+        
 
 def get_course(dept, num):
-    url = "https://selfservice.mypurdue.purdue.edu/prod/bwckctlg.p_disp_course_detail?cat_term_in=201420&subj_code_in={dept}&crse_numb_in={num}".format(dept=dept, num=num)
-    result = urlfetch.fetch(url)
+    host = "https://selfservice.mypurdue.purdue.edu/prod/bwckctlg.p_disp_course_detail"
+    query = "?cat_term_in={term}&subj_code_in={dept}&crse_numb_in={num}".format(term="201420", dept=dept, num=num)
+    result = urlfetch.fetch(host+query)
+    
     if result.status_code == 200:
         tree = html.fromstring(result.content)
         
@@ -89,21 +110,29 @@ def get_course(dept, num):
 
         m = re.search("Typically offered (.*?)\.", text)
         sem = m.group(1).split() if m else ["nomatch"]
-
+        
+        m = re.search("Schedule Types:\s((?:[\w ]+)(?:,[\w ]+)*) \s+", text)
+        form = m.group(1).split(", ") if m else ["nomatch"]
+        
+        # TODO: campus parsing fails on ECE 302
+        
         m = re.search("campuses:\s*(.*)Learn", text,flags=re.DOTALL)
         campus = m.group(1).strip().split("\n\n") if m else ["nomatch"]
 
         # TODO prereq regex and decomosition of prereqs into lists of AND conditions
-        #  prereqList = parsePrereqs(text);
+        
+        # prereqList = parsePrereqs(text);
         # prereq = Prerequisite(parent=ndb.Key('Course'), courses=prereqList)
+        
         # create course entity
-        course = Course(number=int(num), department=dept, description=des, credits=float(cr), semesters=sem, campuses=campus, id=dept + num)
+        course = Course(number=int(num), department=dept, form=form, description=des, credits=float(cr), semesters=sem, campuses=campus, id=dept + num)
         # store course 
         course.put()
         # unnecessary with ndb 
         # return tree.text_content()
-        return campus
+        return text
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/api/', APIHandler)
+    ('/api/?', APIHandler)
 ], debug=True)
